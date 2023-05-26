9d75724bc1b3b4e756bdc20149cb83d064683857
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-05-26 23:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+window.addEventListener('DOMContentLoaded', () => {
+  const targetElement = document.querySelector('#level');
+  let level = 1;
+
+  if (targetElement) {
+    let currentNode = targetElement;
+    while (currentNode.parentNode) {
+      currentNode = currentNode.parentNode;
+      level++;
+    }
+  }
+
+  alert(`The level of the element is: ${level}`);
+});
